86f7946849de3b9abc95e1f40342457646dac2bb
Andrei Badea <badea24@iespabloserrano.com>
2025-10-07 12:00:44 +0200

    second commit

---
+++ saludo.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class saludo {
+
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nombre;
+
+        do {
+            System.out.print("Introduce tu nombre: ");
+            nombre = sc.nextLine().trim(); // quitamos espacios en blanco al inicio y al final
+
+            if (nombre.isEmpty()) {
+                System.out.println("El nombre no puede estar vacío.");
+            }
+
+        } while (nombre.isEmpty());
+
+        // Saludo con el nombre introducido
+        System.out.println("\033[32m ¡Hola, " + nombre + "!");
+        
+        sc.close();
+    }
+}
